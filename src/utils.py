e24005b108b7d281591ffbdc2302f7b44fb669c3
Wok <woctezuma@users.noreply.github.com>
2025-06-28 21:39:56 +0200

    Compute the set intersection between Discord and SteamRep

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,14 @@
 FOLDER_NAME = "data"
+
+
+def compute_intersection(
+    steam_ids: dict[str, str],
+    banned_steam_ids: dict[str, str],
+) -> set[str]:
+    detected = set(steam_ids).intersection(banned_steam_ids)
+    print(f"Detected: {len(detected)}")
+
+    for i in detected:
+        print(f"1. <@{steam_ids[i]}> -> {banned_steam_ids[i]}")
+
+    return detected
